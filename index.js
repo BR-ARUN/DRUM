2087ad1b426a7ecec6201535d7b64ff0a108592b
BR-ARUN <86709852+BR-ARUN@users.noreply.github.com>
2021-10-16 19:18:22 +0530

    update2

--- index.js
+++ index.js
@@ -20,30 +20,30 @@ buttonAnimation(key);
 function makesound(key){
     switch (key) {
         case 'w':
-         var crash = new Audio('../sounds/crash.mp3');
+         var crash = new Audio('sounds/crash.mp3');
          crash.play();
             break;
-        case 'a':  var kickBass = new Audio('../sounds/kick-bass.mp3');
+        case 'a':  var kickBass = new Audio('sounds/kick-bass.mp3');
         kickBass.play();
             
             break;
-        case 's':  var snare = new Audio('/sounds/snare.mp3');
+        case 's':  var snare = new Audio('sounds/snare.mp3');
         snare.play();
             
             break;
-        case 'd':  var tom1 = new Audio('/sounds/tom-1.mp3');
+        case 'd':  var tom1 = new Audio('sounds/tom-1.mp3');
         tom1.play();
             
             break;
-        case 'j':  var tom2 = new Audio('/sounds/tom-2.mp3');
+        case 'j':  var tom2 = new Audio('sounds/tom-2.mp3');
         tom2.play();
             
             break;
-        case 'k':  var tom3 = new Audio('/sounds/tom-3.mp3');
+        case 'k':  var tom3 = new Audio('sounds/tom-3.mp3');
         tom3.play();
             
             break;
-        case 'l':  var tom4 = new Audio('/sounds/tom-4.mp3');
+        case 'l':  var tom4 = new Audio('sounds/tom-4.mp3');
         tom4.play();
             
             break;
